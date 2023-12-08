730ffa9d261d1bee3fa326f6beb2b89eda97ccf1
rukhman <aleksei.rukhmanov@gmx.de>
2023-12-08 21:02:39 +0300

    Roboto font added

--- frontend/cfg/webpack.server.config.js
+++ frontend/cfg/webpack.server.config.js
@@ -45,6 +45,18 @@ module.exports = {
           "less-loader",
         ],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
     ],
   },
   optimization: {
